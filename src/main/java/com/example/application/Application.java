7cca9226e999fa8f0cac7541b6ef17b657d7e4bf
Vlad <vladyslav.balabanov@gmail.com>
2023-10-21 15:03:05 +0200

    remove comments

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -5,15 +5,7 @@ import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
 @SpringBootApplication
-@Theme(value = "mytodo")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
